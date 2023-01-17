10396c3a847fb9ec319c5a64eb7d550952fb3747
AmruthjithSunil <amruthjithsunil@gmail.com>
2023-01-17 21:30:02 +0530

    Starting with expressjs task 3

--- app.js
+++ app.js
@@ -1,15 +1,18 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use((req, res, next) => {
-    console.log('In the middleware');
-    next();
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use('/add-product', (req, res, next) => {
+    res.send('<form action="/product" method="POST"><input type="text" name="title"><input type="text" name="size"><button type="submit">send</button></form>');
 })
 
-app.use((req, res, next) => {
-    console.log('In another middleware');
-    res.send('<h1>Hello from Express!</h1>');
+app.post('/product', (req, res, next) => {
+    console.log('title : ' + req.body.title);
+    console.log('size : ' + req.body.size);
+    res.redirect('/add-product');
 })
 
 app.listen(4000);
