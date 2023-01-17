8f6df28e7753980de693dc40dc871ba8bb9258a7
AmruthjithSunil <amruthjithsunil@gmail.com>
2023-01-17 17:47:48 +0530

    Starting with expressjs task 2

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
-const http = require('http');
+const express = require('express');
 
-const routes = require('./routes');
+const app = express();
 
-const server = http.createServer(routes);
+app.use((req, res, next) => {
+    console.log('In the middleware');
+    next();
+})
 
-server.listen(4000);
+app.use((req, res, next) => {
+    console.log('In another middleware');
+    res.send('<h1>Hello from Express!</h1>');
+})
+
+app.listen(4000);
