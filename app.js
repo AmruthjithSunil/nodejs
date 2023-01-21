eef76e2f3916af05304ae045fa6597acc5baaca8
AmruthjithSunil <amruthjithsunil@gmail.com>
2023-01-21 14:34:26 +0530

    Starting with express task 6

--- app.js
+++ app.js
@@ -1,17 +1,24 @@
 const express = require('express');
+const path = require('path');
 
 const bodyParser = require('body-parser');
 
 const adminRouter = require('./routes/admin');
 const shopRouter = require('./routes/shop');
+const contactRouter = require('./routes/contact');
+const successRouter = require('./routes/success');
 
 const app = express();
 
 app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.use('/admin', adminRouter);
 app.use('/shop', shopRouter);
+app.use('/contactus', contactRouter);
+app.use('/success', successRouter);
 app.use((req, res, next) => {
-    res.status(404).send('<h1>Page not found</h1>');
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 })
 
 app.listen(4000);
--- routes/admin.js
+++ routes/admin.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const path = require('path');
+const rootDir = require('../util/path');
 
 const router = express.Router();
 
 router.get('/add-product', (req, res, next) => {
-    res.send('<form action="/admin/add-product" method="POST"><input type="text" name="title"><input type="text" name="size"><button type="submit">send</button></form>');
+    res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
 })
 
 router.post('/add-product', (req, res, next) => {
